ace6651012329bc5eb827c27ad9a6d7d5e82fcf6
Matías Sepúlveda <71105266+sepuuu@users.noreply.github.com>
2023-04-28 02:06:26 -0400

    actualizacion simplex
    se separan los coeficientes y las variables de la ecuacion

--- metodo_simplex/metodo_simplex.py
+++ metodo_simplex/metodo_simplex.py
@@ -1,1 +1,15 @@
-print("hola")
+import re
+
+# Solicitamos la ecuación al usuario
+ecuacion = input("Ingresa una ecuación del tipo c1 *x1+c2*x2 hasta cn*xn: ")
+
+# Usamos expresiones regulares para encontrar los coeficientes y variables
+coeficientes = re.findall(r'\d+', ecuacion)
+variables = re.findall(r'x\d+', ecuacion)
+
+# Creamos una lista de coeficientes separados
+coeficientes_separados = [float(coeficientes[i]) for i in range(len(coeficientes)) if i % 2 == 0]
+
+# Imprimimos los resultados
+print("Los coeficientes son:", coeficientes_separados)
+print("Las variables son:", variables)
